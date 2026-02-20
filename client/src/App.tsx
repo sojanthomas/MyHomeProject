613ac4f76ce83f02f957ad56f1671b83498734fb
sojan thomas <sojan.thomas@gmail.com>
2026-02-19 16:51:55 -0800

    Layout: two-column main grid; place ActivityScheduler top-right (sticky)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css';
 import AssetManager from './components/AssetManager';
 import AccountManager from './components/AccountManager';
+import ActivityManager from './components/ActivityManager';
 
 function App() {
   return (
     <div className="App">
       <h1>Home Asset Management</h1>
-      <AssetManager />
-      <AccountManager />
+      <div className="main-grid">
+        <div className="left-column">
+          <AssetManager />
+          <AccountManager />
+        </div>
+        <aside className="right-column">
+          <ActivityManager />
+        </aside>
+      </div>
     </div>
   );
 }
